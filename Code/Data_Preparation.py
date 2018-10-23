0fe6175e45ec243c9af3ed68f09a61ba77d382b1
Sumeet Patil <patil.sumeet.v@gmail.com>
2018-10-23 19:00:34 +0530

    main() added

--- Code/Data_Preparation.py
+++ Code/Data_Preparation.py
@@ -2,5 +2,13 @@ import os
 import numpy as np
 import pandas as pd
 
+def main():
+    read_path = 'Data/'
+
+    if os.path.exists(read_path):
+        print("OLA")
+
+    return
+
 if __name__ == '__main__':
     main()
